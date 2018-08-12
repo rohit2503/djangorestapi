b0b12d62f46628269eeb0987fbfa9b7bdac3c674
rohit <rohitjaiswal2503@gmail.com>
2018-08-12 21:02:33 +0530

    1. Add Model Serializer  2.Working demo for rest api

--- snippets/urls.py
+++ snippets/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import views
 
 urlpatterns = [
     url(r'^home/', admin.site.urls),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
